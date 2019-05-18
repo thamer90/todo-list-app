8a4f58debaca744d5724ec245cdf98fc844265ff
thamer90 <thameralmerry@gmail.com>
2019-05-19 01:25:21 +1000

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,226 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './styles/App.css';
+import Item from './components/Item';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+      super(props);
+
+      this.state = { 
+        items: [
+          {
+            value: 'Scale Mount Everest',
+            priority: 'High',
+            completed: false,
+          },
+          {
+            value: 'Pick up dry-cleaning',
+            priority: 'Low',
+            completed: false,
+          },
+          {
+            value: 'Take a Shower',
+            priority: 'Medium',
+            completed: true,
+          }
+        ],
+        value: '',
+        priority: 'Low',
+        completed: false,
+        sortNameDirection: null,
+        sortPriorityDirection: null,
+      };
+
+      this.handleChange = this.handleChange.bind(this);
+      this.handleAddItem = this.handleAddItem.bind(this);
+      this.handleDeleteItem = this.handleDeleteItem.bind(this);
+      this.handleUpdatePriority = this.handleUpdatePriority.bind(this);
+      this.handleSortByName = this.handleSortByName.bind(this);
+      this.handleSortByPriority = this.handleSortByPriority.bind(this);
+  }
+
+  // Handler for input
+  handleChange(e) {
+    this.setState({ value: e.target.value });
+  }
+
+  // Add an item to the list
+  handleAddItem() {
+    if (!this.state.value.length) {
+      return;
+    }
+
+    const newItem = {
+      value: this.state.value,
+      priority: 'Low',
+      completed: false,
+    };
+
+    this.setState(state => ({
+      items: state.items.concat(newItem),
+      value: '',
+      priority: 'Low',
+      completed: false,
+    }));    
+  }
+
+  // Delete an item
+  handleDeleteItem(index) {
+    this.setState(state => ({
+      items: state.items.filter(function(item) { 
+        return item.value !== state.items[index].value
+      })
+    }));
+  }
+
+  // Prioritise an item
+  handleUpdatePriority(event, index) {
+    let items = this.state.items;
+
+    this.setState({
+      items: items.map((item, id) => {
+        if (id === index){
+          return Object.assign({}, item, {
+            priority: event.target.value,
+          })
+        } else {
+          return item;
+        }
+      })
+    })  
+  }
+
+  // Update item status
+  handleUpdateStatus(index) {
+    let items = this.state.items;
+
+    this.setState({
+      items: items.map((item, id) => {
+        if (id === index){
+          return Object.assign({}, item, {
+            completed: !item.completed,
+          })
+        } else {
+          return item;
+        }
+      })
+    })  
+  }
+
+  // Sort list by name
+  handleSortByName() {
+    const items = [...this.state.items].sort((a, b) => {
+      if (a.value.toLowerCase() < b.value.toLowerCase()) return -1;
+      if (a.value.toLowerCase() > b.value.toLowerCase()) return 1;
+      return 0;
+    });
+
+    if (this.state.sortNameDirection !== "asc") {
+      this.setState({ 
+        items: items,
+        sortNameDirection: 'asc',
+      });
+
+      return;
+    }
+
+    this.setState({
+      items: items.reverse(),
+      sortNameDirection: this.state.sortNameDirection === 'asc' ? 'desc' : 'asc',
+    })    
+  }
+
+  // Sort list by priority
+  handleSortByPriority() {
+    let priorityOptions = ["High", "Medium", "Low"]
+    let order = (this.state.sortPriorityDirection === 'desc') ? -1 : 1;
+
+    let items = [...this.state.items].sort((a, b) => {
+        let p1, p2;
+        
+        priorityOptions.forEach(function(row, index){
+        	if (a.priority === row) p1 = index;
+          if(b.priority === row) p2 = index;
+        })
+
+        if (p1 < p2) return -1 * order;
+        if (p1 > p2) return 1 * order;
+
+        return 0;
+    });
+
+    if (this.state.sortPriorityDirection !== "asc") {
+      this.setState({
+        items: items,
+        sortPriorityDirection: 'asc',
+      })
+      return;
+    }
+
+    this.setState({
+      items: items,
+      sortPriorityDirection: this.state.sortPriorityDirection === 'asc' ? 'desc' : 'asc',
+    })
+  }
+
+  render() {
+      const { items } = this.state;
+      console.log(this.state);
+
+      let hasItems = items !== undefined || items.length > 0;
+      let completedItemsCount = items.reduce(function (n, item) {
+        return n + +(item.completed === true);
+      }, 0);
+    
+      return (
+          <div className="App">
+              <h1>Todo List</h1>
+              <div className="add-task-container">
+                <input type="text" placeholder="Start typing to add a task.." onChange={this.handleChange} value={this.state.value}></input>
+                <button onClick={this.handleAddItem} className="btn-floating waves-effect waves-light red">
+                  <i className="material-icons">add</i>
+                </button>
+              </div>
+              {
+                hasItems &&
+                <table className="striped">
+                  <thead>
+                    <tr>
+                        <th>Task</th>
+                        <th>Priority</th>
+                        <th>Status</th>
+                        <th>Priority</th>
+                        <th>Actions</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {
+                      items.map((item, index) => (
+                          <Item key={`items-${index}`}
+                                index={index}
+                                value={item.value}
+                                priority={item.priority}
+                                completed={item.completed}
+                                updateStatus={() => this.handleUpdateStatus(index)}
+                                updatePriority={(e) => this.handleUpdatePriority(e, index)}
+                                deleteItem={() => this.handleDeleteItem(index)}
+                          />
+                      ))
+                    }
+                  </tbody>
+                </table>                
+              }
+              <div className="actions-section">
+                <button onClick={() => this.handleSortByName()} className="waves-effect waves-light btn-small">Sort by Name</button>
+                <button onClick={() => this.handleSortByPriority()} className="waves-effect waves-light btn-small">Sort by Priority</button>
+              </div>
+              <div className="summary-section">
+                <p>Number of Tasks:{this.state.items.length}</p>
+                <p>Completed Tasks:{completedItemsCount}</p>
+              </div>
+          </div>
+      );
+  }
 }
 
 export default App;
---
+++ src/components/Item/index.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Item = (props, index) => {
+    const priorityOptions = ['Low', 'Medium', 'High'];
+
+    let status = props.completed ? 'Completed' : 'Pending';
+    let statusButton = props.completed?
+        <button onClick={props.updateStatus} className="waves-effect waves-light btn-small red"><i className="material-icons">close</i></button>
+        : <button onClick={props.updateStatus} className="waves-effect waves-light btn-small green"><i className="material-icons">check</i></button>;
+
+    return (
+        <tr index={index} className="item">
+            <td>{props.value}</td>
+            <td>{props.priority}</td>
+            <td>{status}</td>
+            <td>
+                <select value={props.priority} onChange={props.updatePriority}>
+                    {
+                        priorityOptions.map((option, index) => (
+                            <option 
+                                key={`priority-${index}`}
+                                value={option}>
+                                {option}
+                            </option>
+                        ))
+                    }
+                </select>             
+            </td>
+            <td>
+                {statusButton}
+                <button onClick={props.deleteItem} className="waves-effect waves-light btn-small grey"><i className="material-icons">delete</i></button>               
+            </td>
+        </tr>
+    );
+}
+
+Item.propTypes = {
+    value: PropTypes.string,
+    priority: PropTypes.string,
+    selectedItem: PropTypes.string,
+    completed: PropTypes.bool,
+    updateStatus: PropTypes.func.isRequired,
+    updatePriority: PropTypes.func.isRequired,
+    deleteItem: PropTypes.func.isRequired,
+};
+
+Item.defaultProps = {
+    value: '',
+    priority: 'Low',
+    completed: false,
+};
+
+export default Item;
